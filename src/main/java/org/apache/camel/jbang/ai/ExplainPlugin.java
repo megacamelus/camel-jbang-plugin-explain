4f1d3e2101e83395fcfc2a971de14ee392d91250
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-06-25 13:27:42 +0200

    Updated to the latest plugin annotation

--- src/main/java/org/apache/camel/jbang/ai/ExplainPlugin.java
+++ src/main/java/org/apache/camel/jbang/ai/ExplainPlugin.java
@@ -1,12 +1,11 @@
 package org.apache.camel.jbang.ai;
 
 import org.apache.camel.dsl.jbang.core.commands.CamelJBangMain;
-import org.apache.camel.dsl.jbang.core.commands.ExplainCommand;
 import org.apache.camel.dsl.jbang.core.common.CamelJBangPlugin;
 import org.apache.camel.dsl.jbang.core.common.Plugin;
 import picocli.CommandLine;
 
-@CamelJBangPlugin("camel-jbang-plugin-explain")
+@CamelJBangPlugin(name = "camel-jbang-plugin-explain", firstVersion = "4.7.0-SNAPSHOT")
 public class ExplainPlugin implements Plugin {
     @Override
     public void customize(CommandLine commandLine, CamelJBangMain main) {
